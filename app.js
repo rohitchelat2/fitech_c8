0f34f23a11772b1c93a030882ac6df68f6af4fb4
rohitchelat2 <157241551+rohitchelat2@users.noreply.github.com>
2024-09-02 13:06:22 +0300

    second_commit

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 import { serve } from "./deps.js";
+
 const handleRequest = (request) => {
         return new Response("Seeking truths beyond meaning of life, you will find 43.");
 };
 
-serve(handleRequest, { port: 7777 });
+serve(handleRequest, { port: 8000 });
 
