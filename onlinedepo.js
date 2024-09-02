df3083cbca6f0ff2cdf8a144d867708caf228c53
rohitchelat2 <157241551+rohitchelat2@users.noreply.github.com>
2024-09-02 11:04:55 +0300

    first commit

--- onlinedepo.js
+++ onlinedepo.js
@@ -1,10 +1,7 @@
 import { serve } from "https://deno.land/std@0.222.1/http/server.ts";
 
 const handleRequest = (request) => {
-
         return new Response("Seeking truths beyond meaning of life, you will find 43.");
-    
-  
 };
 
 serve(handleRequest, { port: 7777 });
